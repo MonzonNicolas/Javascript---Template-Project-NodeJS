fe6ddb6a34f8830e792e9b1f62345099f7052fa1
Nicolas_Monzon <monzon.nicolas.2019@gmail.com>
2019-01-29 15:19:36 -0300

    Hello World

--- app.js
+++ app.js
@@ -1,27 +1,8 @@
 const express = require('express')
 const app = express()
 
-app.get('/', (req, res) => {
-	res.end("finalizando")
+app.use(express.static('assets')) //Usa los arhicvos estáticos de directorio indicado
+
+app.listen(3000, () => {
+	console.log('Escuchando en el puerto 3000')
 })
-
-/*Documentación: //Obtenido de StackOverflow
-
-res.send([body])
-Sirve para enviar la respuesta HTTP. Ejemplos:
-res.send(new Buffer('buffer'));
-res.send('<p>ejemplo html</p>');
-res.status(404).send('Error 404');
-res.status(500).send({ error: 'error 500' });
-res.send({ ejemplo: 'json' });
-
-En http.ServerResponse de Node Core tenemos el método response.end(), este es
-el equivalente de res.end(). Se usa para finalizar rapido sin datos:
-res.end();
-res.status(404).end();
-
-Mas informacion:
-http://expressjs.com/en/4x/api.html#res.send
-http://expressjs.com/en/4x/api.html#res.end
-
-*/
